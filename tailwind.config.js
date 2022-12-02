e332105d0f38a3afbe9b76dd7e360dac62e95364
ikyro <ikyroch@gmail.com>
2022-12-02 02:39:17 -0300

    use tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,16 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        pixel: 'var(--pixel-font)',
+        'wild-words': 'var(--wild-words-font)',
+        poopins: 'var(--poppins-font)',
+      },
+    },
   },
   plugins: [],
 }
